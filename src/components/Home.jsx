3581ae99498b5946260489d617440d2095d1cfa1
Vonney <davonnejv@gmail.com>
2023-08-23 05:05:45 -0500

    tidying up code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,6 @@ import './HomePage.css'
 export default function Home() {
 
   return (
-  
         <div className="image-container">   
          <img src="/images/homeImage.jpg" className="homePic" />
          <button className="shopAll">Shop All</button>
