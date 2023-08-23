01f5bdd0ca5e33b0b04b0772e946028252b4f249
Vonney <davonnejv@gmail.com>
2023-08-23 04:10:27 -0500

    adding a couple images to image folder

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,14 +3,15 @@ import './HomePage.css'
 
 export default function Home() {
 
-
   return (
   
         <div className="image-container">   
          <img src="/images/homeImage.jpg" className="homePic" />
-         <button className="shopAll">Shop All</button><br/><br/>
+         <button className="shopAll">Shop All</button>
          <p>Check out latest trends from clothing to technology</p>
          </div>
-  
   )
 }
+
+
+
