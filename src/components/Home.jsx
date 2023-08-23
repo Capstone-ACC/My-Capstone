6ded9d4ad05d238d5f3949952c95b5f05bfbf3b5
Vonney <davonnejv@gmail.com>
2023-08-23 03:09:19 -0500

    adding button to shop all items on top of image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,9 @@ export default function Home() {
 
 
   return (
-    <div>   
+    <div className="image-container">   
         <img src="/images/homeImage.jpg" className="homePic" />
+        <button className="shopAll">Shop All</button>
 
         
     </div>
