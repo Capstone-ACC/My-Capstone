173f15505879f061b0f1eb25129593691b574972
Vonney <davonnejv@gmail.com>
2023-08-24 20:42:44 -0500

    adding function for when image is clicked on

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,9 +3,15 @@ import './HomePage.css'
 
 export default function Home() {
 
+  function changeImage() {
+    alert("test")
+  }
+
+  changeImage();
+
   return (
         <div className="image-container">   
-         <img src="/images/homeImage.jpg" className="homePic" />
+         <img src="/images/homeImage.jpg" className="homePic" id="homeImage" onClick={changeImage}/>
          <button className="shopAll">Shop All</button>
          <p>Check out latest trends from clothing to technology</p>
          </div>
