75cc47aed717fcc945639b645b1d56d71262ab9d
Vonney <davonnejv@gmail.com>
2023-08-24 23:12:25 -0500

    addingbasic login and password front-end

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './LoginStyles.css'
 
 export default function Login() {
+
+
   return (
-    <div>Login</div>
+    <div className="login-container">
+        <h2>Login</h2>
+
+        <form>
+            <label>
+                Username:
+                <input 
+                value="username" 
+                className="input-login"/>
+            </label>
+
+            <label>
+                Password:
+                <input 
+                value="password"
+                className="input-login" />
+            </label>
+        </form>
+        
+    </div>
   )
 }
