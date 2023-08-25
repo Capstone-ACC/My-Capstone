c2a6667fe31bea96abdb5e592879c7f1760e34f2
Vonney <davonnejv@gmail.com>
2023-08-25 14:57:05 -0500

    adding pseudocode and format getAllProducts fetch

---
+++ src/components/api/index.js
@@ -0,0 +1,15 @@
+/*fetch all products*/
+//async function
+//try catch
+//set variable to the api url, fetch, await
+//set the variable to set as json, which is also declared to a variable
+//console log the new variable
+// return 
+//catch error
+export const getAllProducts = async () => {
+    try {
+
+    } catch (error) {
+        console.error("Error:", error)
+    }
+}
