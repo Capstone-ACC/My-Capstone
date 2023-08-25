0028024e534227a0e83c1791f2a683d31462bb91
Vonney <davonnejv@gmail.com>
2023-08-25 05:12:35 -0500

    one last edit, due to changing name of component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   const navigate = useNavigate();
 
   function goToAllProducts() {
-    navigate('/allProducts')
+    navigate('/main-all-products')
   }
 
   return (
