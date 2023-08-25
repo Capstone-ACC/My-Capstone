ea12a63899f73cd98122ff6d896d052e50f8b910
Vonney <davonnejv@gmail.com>
2023-08-25 15:14:21 -0500

    was able to fetch all the products and console.log them for now

--- src/components/api/index.js
+++ src/components/api/index.js
@@ -8,6 +8,10 @@
 //catch error
 export const getAllProducts = async () => {
     try {
+        const response = await fetch("https://fakestoreapi.com/products")
+        const result = await response.json()
+        console.log("All Products:",result )
+        return result.products
 
     } catch (error) {
         console.error("Error:", error)
