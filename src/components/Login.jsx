d34350100eda7d4ec19217cca9967151ff3af08e
Vonney <davonnejv@gmail.com>
2023-08-27 16:50:12 -0500

    seeing if I can validation to my login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,3 +1,100 @@
+// import React, { useState } from 'react'
+// import { useNavigate } from 'react-router-dom'
+// import './Login-Register-Styles.css'
+
+// export default function Login({setToken}) {
+//    const [username, setUsername] = useState("")
+//    const [password, setPassword] = useState("")
+
+//     //useNavigate
+//    const navigate = useNavigate()
+
+//    const inputUsername = (e) => {
+//         setUsername(e.target.value)
+//     }
+
+//    const inputPassword = (e) => {
+//         setPassword(e.target.value)
+//     }
+
+//     const handelSubmit = async (e) => {
+//         e.preventDefault();
+//         alert("You are now logged in, check console.log for token")
+//     }
+
+//     //fetch for login 
+//     const handelLogin = async () => {
+//         try {
+//             const response = await fetch ("https://fakestoreapi.com/auth/login", {
+//                 method: "POST",
+//                 headers: {
+//                     'Content-Type': 'application/json'
+//                 },
+//                 body: JSON.stringify({
+//                     username: "mor_2314",
+//                     password: "83r5^_"
+//                 })
+//             })
+
+//             const result = await response.json();
+//             console.log("Token:", result)
+//             console.log(`Welcome, ${username}, browse through our products to see whats right for you!`)
+
+//             if(result.token) {
+//                 localStorage.setItem('token', result.token)
+//                 setToken(result.token)
+//             } else {
+//                 console.error("Try again, no token found for this user")
+//             }
+//         } catch (error) {
+//             console.error('There is an error:', error)
+//         }
+//         navigate('/main-all-products')
+//     }
+
+//   return (
+//     <>
+//       <br/>
+//       <hr />
+//         <div className="login-container">
+//             <h2>Login</h2>
+
+//              <form onSubmit={handelSubmit} class="was-validated">
+//                  <label>
+//                  Username:
+
+//                  <input 
+//                  value={username}
+//                  className="input-login"
+//                  placeholder="Required"
+//                  onChange={inputUsername} />
+//                 </label>
+
+//                 <div class="valid-feedback">Username is Valid</div>
+//                 <div class="invalid-feedback">Please fill out this field</div>
+
+//                  <label>
+//                  Password:
+
+//                  <input 
+//                  value={password}
+//                  type="password"
+//                  className="input-login"
+//                  placeholder="Required"
+//                  onChange={inputPassword} />
+//                 </label>
+
+//                  <button className="login-button" onClick={handelLogin}>Login</button>
+//              </form>
+//        </div>
+
+//        <img src="/images/laptop.jpg" className="loginImg" />
+//   </>
+//   )
+// }
+
+
+
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import './Login-Register-Styles.css'
@@ -5,6 +102,7 @@ import './Login-Register-Styles.css'
 export default function Login({setToken}) {
    const [username, setUsername] = useState("")
    const [password, setPassword] = useState("")
+   const [isValid, setValid] = useState(true);
 
     //useNavigate
    const navigate = useNavigate()
@@ -67,8 +165,12 @@ export default function Login({setToken}) {
                  value={username}
                  className="input-login"
                  placeholder="Required"
-                 onChange={inputUsername} />
+                 onChange={inputUsername} 
+                 onBlur={handelValidation} />
                 </label>
+
+                <div class="valid-feedback">Username is Valid</div>
+                <div class="invalid-feedback">Please fill out this field</div>
 
                  <label>
                  Password:
@@ -78,7 +180,8 @@ export default function Login({setToken}) {
                  type="password"
                  className="input-login"
                  placeholder="Required"
-                 onChange={inputPassword} />
+                 onChange={inputPassword}
+                 onBlur={handelValidation} /> 
                 </label>
 
                  <button className="login-button" onClick={handelLogin}>Login</button>
@@ -89,6 +192,3 @@ export default function Login({setToken}) {
   </>
   )
 }
-
-
-
