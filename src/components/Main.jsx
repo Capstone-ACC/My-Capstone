52b016e76435e7feb8c48011cf96bbd1d221d5be
Vonney <davonnejv@gmail.com>
2023-08-28 22:05:10 -0500

    setting up container for the product info

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,7 +6,7 @@ import './Main-SearchBar.css'
 
 export default function Main() {
 const [products, setProducts] = useState([])
-// const [searchedProducts, setSearch] = useState("")
+const [searchedProducts, setSearchProducts] = useState("")
 
 useEffect(() => {
     const fetchProducts = async () => {
@@ -22,6 +22,10 @@ useEffect(() => {
     fetchProducts()
 }, [])
 
+const handelSearchInput = (searchValue) => {
+    setSearchProducts(searchValue)
+}
+
     return (
         <>
          <br/>
@@ -29,13 +33,21 @@ useEffect(() => {
     
          <section>
             <h3>Customize Your Style and Tech</h3>
-            <SearchBar />
+            <SearchBar value={searchedProducts} onChange={handelSearchInput} />
 
-
-           
-
-
-         </section>
+            <div className="products-container">
+                <div className = "my-box">
+                    <div className="content">
+                        <h4>Title: </h4>
+                        <p1>Description: </p1>
+                        <p1>Category: </p1>
+                        <p1>ID: </p1>
+                        <p1>Price: </p1>
+                        <h5>Image: </h5>
+                    </div>
+                </div>
+            </div> {/* last div to product-container */}
+        </section>
         </>
       )
     }
