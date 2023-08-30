e6006eef84992e3582df077218fe4c08a5a10515
Vonney <davonnejv@gmail.com>
2023-08-30 12:07:50 -0500

    adding fetch for single product, and psesudocoding other fetches from api

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { useState, useEffect} from 'react'
+import { getSingleProduct } from './api'
 
 export default function SingleProduct() {
+  const [singleProduct, setSingleProduct] = useState("")
+
+  useEffect(() => {
+    const fetchSingleProduct = async () => {
+      try {
+        const mySingleProduct = await getSingleProduct();
+        setSingleProduct(mySingleProduct);
+
+      } catch(error) {
+        console.error("Error:", error)
+      }
+    }
+
+    fetchSingleProduct()
+
+  }, [])
+
   return (
-    <div>Single Product</div>
+    <div>
+      Single Product
+    </div>
   )
 }
--- src/components/api/index.js
+++ src/components/api/index.js
@@ -1,12 +1,4 @@
 /*fetch all products*/
-
-//async function
-//try catch
-//set variable to the api url, fetch, await
-//set the variable to set as json, which is also declared to a variable
-//console log the new variable
-// return products
-//catch error
 export const getAllProducts = async () => {
     try {
         const response = await fetch("https://fakestoreapi.com/products")
@@ -18,3 +10,53 @@ export const getAllProducts = async () => {
         console.error("Error:", error)
     }
 }
+
+/*fetch single product*/
+export const getSingleProduct = async () => {
+    try {
+        const response = await fetch("https://fakestoreapi.com/products/1")
+        const result = await response.json()
+        console.log("Single Product:", result)
+        return result;
+    } catch (error) {
+        console.error("Error:", error)
+    }
+}
+
+/*sort products*/
+// https://fakestoreapi.com/products?sort=desc
+
+
+
+/*get all categories*/
+// https://fakestoreapi.com/products/categories
+
+
+
+
+/*get all cart*/
+// https://fakestoreapi.com/carts
+
+
+
+
+/*get single cart*/
+// https://fakestoreapi.com/carts/5
+
+
+
+
+/*add a new product in cart*/
+ //turn into async
+//  fetch('https://fakestoreapi.com/carts',{
+//             method:"POST",
+//             body:JSON.stringify(
+//                 {
+//                     userId:5,
+//                     date:2020-02-03,
+//                     products:[{productId:5,quantity:1},{productId:1,quantity:5}]
+//                 }
+//             )
+//         })
+//             .then(res=>res.json())
+//             .then(json=>console.log(json))
