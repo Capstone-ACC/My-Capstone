bd9a586917b305c86f3d5de7468b89b6de9addb8
Vonney <davonnejv@gmail.com>
2023-08-31 18:18:44 -0500

    tididying up code and last styles on top-header

--- src/components/api/index.js
+++ src/components/api/index.js
@@ -65,9 +65,7 @@ export const singleCart = async (id) => {
 }
 
 
-
 /*add a new product in cart*/
- //turn into async
 export const addCart = async () => {
     try {
         const response = await fetch("https://fakestoreapi.com/carts", {
