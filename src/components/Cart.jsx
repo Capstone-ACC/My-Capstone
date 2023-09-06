4f9c7e67bff46ec88f220b6dade703c8a343e2d7
Vonney <davonnejv@gmail.com>
2023-09-06 05:09:54 -0500

    was able to add functionallity to cart, mapped the state of the cart, and some styles

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,14 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import {  getAllCarts } from './api'
 import './Cart.css'
+import { CartContext } from '../Context/Context'
 
-export default function Cart({ cartItems }) {
+export default function Cart() {
   const [allCart, setAllCart] = useState({})
+
+  const myCart = useContext(CartContext)
+  const state = myCart.state;
+  const dispatch = state.dispatch
 
   //get all carts
   useEffect(() => {
@@ -26,9 +31,20 @@ export default function Cart({ cartItems }) {
      <hr />
 
      <h5>My Cart</h5>
+    
      <div className="cart-container"> 
-       
-
+        {state.map((item, index) => {
+          return (
+            <div className="myItems" key={index}>
+              <img src={item.image} className="cartImg" />
+              <span>{item.title}</span>
+              <span>${item.price}</span>
+              <button>-</button>
+              <button>+</button>     
+            </div>
+          )
+        })}
+        <button className="cartBackToProducts">Back To Products</button>
      </div>
     </>
   )
