1f0f7f2c766da4c6d5ff5f0c44f82e0311978be6
Vonney <davonnejv@gmail.com>
2023-09-11 06:59:12 -0500

    adding if cart is empty to display a message

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,7 +2,7 @@ import { useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import { CartContext } from "../Context/Context";
 import { useNavigate } from "react-router-dom";
-import { saveCartToLocalStorage, getCartFromLocalStorage} from '../Context/CartUtils'
+import { saveCartToLocalStorage,getCartFromLocalStorage,} from "../Context/CartUtils";
 import "./css/Cart-Checkout.css";
 
 export default function Cart() {
@@ -10,15 +10,15 @@ export default function Cart() {
   const { state, dispatch } = myCart;
 
   useEffect(() => {
-    const cartData = getCartFromLocalStorage()
+    const cartData = getCartFromLocalStorage();
     if (cartData.length > 0) {
-      dispatch({type: "LOAD_CART", payload: cartData})
+      dispatch({ type: "LOAD_CART", payload: cartData });
     }
-  }, [dispatch])
+  }, [dispatch]);
 
   //total price
   const totalPrice = state.reduce((total, item) => {
-    return total + (item.price * item.quantity);
+    return total + item.price * item.quantity;
   }, 0);
 
   //use navigate
@@ -36,64 +36,70 @@ export default function Cart() {
       <div className="cart-container">
         <h5>My Cart</h5>
 
-        {state.map((item, index) => {
-          return (
-            <div className="myItems" key={index}>
-              <img src={item.image} className="cartImg" />
-              <span>{item.title}</span>
-              <span>${item.price}</span>
+        {state.length === 0 ? (
+          <span style={{ fontSize: "22pt" }}>Cart is empty for now</span>
+        ) : (
+          <>
+            {state.map((item, index) => {
+              return (
+                <div className="myItems" key={index}>
+                  <img src={item.image} className="cartImg" />
+                  <span>{item.title}</span>
+                  <span>${item.price}</span>
 
-              <button
-                onClick={() => {
-                  console.log("Decreased Quantity:", item);
-                  dispatch({ type: "DECREASE", payload: item });
-                  saveCartToLocalStorage([...state]);
-                }}
-                className="cart-buttons"
-              >
-                -
+                  <button
+                    onClick={() => {
+                      console.log("Decreased Quantity:", item);
+                      dispatch({ type: "DECREASE", payload: item });
+                      saveCartToLocalStorage([...state]);
+                    }}
+                    className="cart-buttons"
+                  >
+                    -
+                  </button>
+
+                  <span>{item.quantity}</span>
+
+                  <button
+                    onClick={() => {
+                      console.log("Increased Quantity:", item);
+                      dispatch({ type: "INCREASE", payload: item });
+                      saveCartToLocalStorage([...state]);
+                    }}
+                    className="cart-buttons"
+                  >
+                    +
+                  </button>
+
+                  <button
+                    onClick={() => {
+                      console.log("Deleted Item:", item);
+                      dispatch({ type: "DELETE", payload: item });
+                      saveCartToLocalStorage([...state]);
+                    }}
+                    className="cart-buttons"
+                  >
+                    Delete
+                  </button>
+
+                  <hr />
+                </div>
+              );
+            })}
+
+            <span className="total-price">Total: ${totalPrice.toFixed(2)}</span>
+
+            <div className="add-more-bts-and-checkout">
+              <button className="cartBackToProducts" onClick={goBackToProducts}>
+                Add More Items
               </button>
 
-              <span>{item.quantity}</span>
-
-              <button
-                onClick={() => {
-                  console.log("Increased Quantity:", item);
-                  dispatch({ type: "INCREASE", payload: item });
-                  saveCartToLocalStorage([...state]);
-                }}
-                className="cart-buttons"
-              >
-                +
+              <button>
+                <Link to="/checkout">Check Out</Link>
               </button>
-
-              <button
-                onClick={() => {
-                  console.log("Deleted Item:", item);
-                  dispatch({ type: "DELETE", payload: item });
-                  saveCartToLocalStorage([...state]);
-                }}
-                className="cart-buttons"
-              >
-                Delete
-              </button>
-
-              <hr />
             </div>
-          );
-        })}
-
-        <span className="total-price">Total: ${totalPrice.toFixed(2)}</span>
-
-       <div className="add-more-bts-and-checkout">
-         <button className="cartBackToProducts" onClick={goBackToProducts}>
-            Add More Items
-          </button>
-
-          <button>
-            <Link to="/checkout">Check Out</Link>
-          </button>
-       </div>
+          </>
+        )}
       </div>
     </>
   );
