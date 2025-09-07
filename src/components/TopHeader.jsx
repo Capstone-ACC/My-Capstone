1e71cdba67008b1368c1fa4658ed011b6168c820
Davonne Vigil <davonnejv@gmail.com>
2025-09-06 21:51:34 -0500

    update: top header component, mobile responsiveness

--- src/components/TopHeader.jsx
+++ src/components/TopHeader.jsx
@@ -13,7 +13,6 @@ export default function TopHeader() {
     navigate("/cart");
   };
 
-  // //handle logout
   const handleLogOut = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("username");
@@ -21,26 +20,22 @@ export default function TopHeader() {
   };
 
   return (
-    <>
-      <header className="top-header p-2">
-        <GetStarted />
-        <span className="cart-icon">
-          <i className="fa fa-cart-plus cart-icon" onClick={goToCart}>
-            {" "}
-            {cartItems.length > 0 && (
-              <span className="cart-item-count">{cartItems.length}</span>
-            )}
-          </i>
-        </span>
-        <div className="mb-4">
-          <Link to="/login" onClick={handleLogOut} className="login">
-            Logout
-          </Link>
-          <Link to="/login" className="login">
-            Login
-          </Link>
-        </div>
-      </header>
-    </>
+    <header className="p-2 bg-black text-white">
+      <GetStarted />
+
+      <span className="cart-icon">
+        <i className="fa fa-cart-plus cart-icon" onClick={goToCart}>
+          {cartItems.length > 0 && (
+            <span className="cart-item-count">{cartItems.length}</span>
+          )}
+        </i>
+      </span>
+      <Link to="/login" className="login">
+        Login
+      </Link>
+      <Link to="/login" className="login" onClick={handleLogOut}>
+        Logout
+      </Link>
+    </header>
   );
 }
